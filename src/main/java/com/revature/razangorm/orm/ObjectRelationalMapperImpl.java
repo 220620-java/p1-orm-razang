1d26864697df0e479a329f60e7c0fe817dbb49c0
raza <raza.ghulam2021@gmail.com>
2022-07-13 21:49:00 -0700

    Partially implemented create mothod of ObjectRelationalMapperImpl class

--- src/main/java/com/revature/razangorm/orm/ObjectRelationalMapperImpl.java
+++ src/main/java/com/revature/razangorm/orm/ObjectRelationalMapperImpl.java
@@ -1,10 +1,44 @@
 package com.revature.razangorm.orm;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.revature.razang.utilities.ConnectionObject;
+
+
 public class ObjectRelationalMapperImpl implements ObjectRelationalMapper {
 
+	private ConnectionObject connObj = ConnectionObject.getConnectionUtil();
+	private QueryMapper mapper = new QueryMapper(); 
+	List<Object> objects = new ArrayList<>();
+	
 	@Override
-	public Object create(Object obj) {
+	public  Object create(Object obj) {
 		// TODO Auto-generated method stub
+		try (Connection conn = connObj.getConnection()) {
+			conn.setAutoCommit(false);
+			String sql = mapper.createObject(obj, "customer"); 
+			PreparedStatement st = conn.prepareStatement(sql);
+			Field[] fields = mapper.getFields(obj.getClass()); 
+			for (Field field: fields) {
+				System.out.println(field);
+			}
+			
+			
+			
+			
+		} catch(SQLException e) {
+			e.getStackTrace(); 
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
@@ -26,5 +60,7 @@ public class ObjectRelationalMapperImpl implements ObjectRelationalMapper {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
 
 }
