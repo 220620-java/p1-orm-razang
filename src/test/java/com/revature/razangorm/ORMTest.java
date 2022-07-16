0ea1961eaf96e96d1c58d7f9b9c8c9423886e6f7
Colby Tang <cobtang@gmail.com>
2022-07-15 21:01:13 -0500

    Tried mocking the findall test case

--- src/test/java/com/revature/razangorm/ORMTest.java
+++ src/test/java/com/revature/razangorm/ORMTest.java
@@ -2,19 +2,36 @@ package com.revature.razangorm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.sql.Statement;
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.revature.razang.utilities.ConnectionObject;
 import com.revature.razangorm.models.Account;
 import com.revature.razangorm.models.Customer;
 import com.revature.razangorm.orm.ObjectRelationalMapper;
 import com.revature.razangorm.orm.ObjectRelationalMapperImpl;
 
+@ExtendWith (MockitoExtension.class)
 public class ORMTest {
-	
-	ObjectRelationalMapper test = new ObjectRelationalMapperImpl(); 
+	@InjectMocks
+	ObjectRelationalMapper test = new ObjectRelationalMapperImpl();
+
+	@Mock
+	private ConnectionObject connObj;
+
+	@Mock
+	private Statement st;
 
 //    @Test
 //    public void updateObjectByEmail () {
@@ -42,18 +59,29 @@ public class ORMTest {
 	}
 	
 	@Test 
-	public void findAll() {
+	public void findAll() throws SQLException {
 		Customer customer = new Customer(1, "raza@gmail.com", new Date(1999-01-01),
         		"razaghulam", "10210101010", "12345");
-		List<Object> obj = test.findAll(customer, "customer");
-		System.out.println(obj);
+
+		Connection jdbcConnection = Mockito.mock(Connection.class);
+		Statement statement = Mockito.mock(Statement.class);
+		ResultSet result = Mockito.mock(ResultSet.class);
+
+		
+		// Mockito.when (connObj.getConnection()).thenReturn(jdbcConnection);
+		// Mockito.when (jdbcConnection.createStatement()).thenReturn(statement);
+		// Mockito.when(result.next()).thenReturn(true).thenReturn(false);
+
+		//Unfinished mocking
+		// List<Object> obj = test.findAll(customer, "customer");
+		// System.out.println(obj);
 		
 	}
 	
 	@Test
 	public void testFindAllAccount() {
 		Account account = new Account("123344556", 0.00, 1 );
-		List<Object> accObjs = test.findAll(account, "BankAccount"); 
-		System.out.println(accObjs);
+		// List<Object> accObjs = test.findAll(account, "BankAccount"); 
+		// System.out.println(accObjs);
 	}
 }
